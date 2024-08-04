4d878bd09121dc92bcb4f91607c1bb040594f73e
Ajmal M <ajmal@mindster.in>
2024-08-04 08:49:05 +0530

    initial commit

---
+++ server.php
@@ -0,0 +1,72 @@
+<?php
+require 'vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        // Store the new connection
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf(
+            'Connection %d sending message "%s" to %d other connection%s' . "\n"
+            ,
+            $from->resourceId,
+            $msg,
+            $numRecv,
+            $numRecv == 1 ? '' : 's'
+        );
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // Send the message to all clients except the sender
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        // The connection is closed, remove it from the clients list
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+// Run the server
+$loop = React\EventLoop\Loop::get();
+$pusher = new Chat();
+$webSock = new React\Socket\SocketServer('0.0.0.0:8080', [], $loop);
+$webServer = new Ratchet\Server\IoServer(
+    new Ratchet\Http\HttpServer(
+        new Ratchet\WebSocket\WsServer(
+            $pusher
+        )
+    ),
+    $webSock
+);
+
+echo "WebSocket server running at ws://localhost:8080\n";
+$loop->run();
+
